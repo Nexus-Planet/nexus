9abf545f57d12ce067cac9ffb86cb6b7fbcdcd83
reiyuchan <iwakura.rei.tk@gmail.com>
2026-02-21 05:06:19 +0200

    feat(server): add proper middleware log to server routes

---
+++ internal/server/http.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/bytedance/sonic"
+)
+
+func JSON(w http.ResponseWriter, status int, v interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	err := sonic.ConfigDefault.NewEncoder(w).Encode(v)
+	if err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusBadRequest)
+		return err
+	}
+	return nil
+}
+
+func Text(w http.ResponseWriter, status int, s string) error {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(status)
+	_, err := w.Write([]byte(s))
+	if err != nil {
+		return err
+	}
+	return nil
+}
